e444ae2d14e48d5dfefab6105e119722604889ee
shadowmoose <theshadowmoose@gmail.com>
2018-06-15 16:52:09 -0700

    Adds basic seeking, and implements Cropping.

--- web/control.js
+++ web/control.js
@@ -7,6 +7,7 @@ let video_size = {'w': 0, 'h': 0};
 let loaded = false;
 let time_start = 10;
 let time_end = 12;
+let crop = [null, null];
 
 
 $(function() {
@@ -37,7 +38,6 @@ $(function() {
 		update_slider_fields();
 	}).bind('loadeddata', function(e) {
 		e.target.play();  // start playing
-		update(e.target); //Start rendering
 	}).on('pause', (e)=>{
 		console.log('Paused: ', e.target.currentTime)
 	});
@@ -45,6 +45,49 @@ $(function() {
 	$('.slider_control').on('change', (e)=>{
 		set_slider();
 	});
+
+	let drawing = false;
+	$("#canv").mousedown((e)=>{
+		let pos = getMousePos(canvas, e);
+		drawing = true;
+		console.log('click', pos);
+		crop = [pos, null]
+	}).mousemove(function(e) {
+		if(!drawing)
+			return;
+		let pos = getMousePos(canvas, e);
+		crop = [crop[0], pos];
+	}).on('mouseup', function(e) {
+		if(!drawing)
+			return;
+		let pos = getMousePos(canvas, e);
+		console.log('Mouse Up', pos);
+		crop = [crop[0], pos];
+		drawing = false;
+		if(crop[0].x === crop[1].x && crop[0].y === crop[1].y)
+			crop = [null, null];
+		console.log(crop);
+	});
+
+	$('.slider_time_pos').mousedown((e)=>{
+		let ele = e.target;
+		let last_pos = e.clientX;
+		function mup(e, ele){
+			console.log('up');
+			document.onmousemove = null;
+			document.onmouseup = null;
+		}
+		function mmov(e, ele){
+			let delta = e.clientX - last_pos;
+			console.log('Delta:', delta);
+			last_pos = e.clientX;
+			let total_percent = (ele.offsetLeft+delta)/ele.parentElement.offsetWidth;
+			console.log(total_percent);
+			video.currentTime = video.duration * total_percent
+		}
+		document.onmousemove = (e)=>{mmov(e, ele)};
+		document.onmouseup = (e)=>{mup(e, ele)};
+	})
 });
 
 
@@ -68,18 +111,59 @@ function set_slider(){
 }
 
 
+function getMousePos(canvas, evt) {
+	let rect = canvas.getBoundingClientRect();
+	return {
+		x: (evt.clientX - rect.left) / rect.width,
+		y: (evt.clientY - rect.top) / rect.height
+	};
+}
+
+function unscale(coords, rect){
+	return{
+		'x': coords.x * rect.width,
+		'y': coords.y * rect.height
+	}
+}
+
+function pause_toggle(){
+	console.log('toggle play');
+	if(video.paused){
+		video.play();
+		$(".play_toggle").html('&#10074;&#10074;')
+	}else{
+		video.pause();
+		$(".play_toggle").html('&#9654;')
+	}
+}
+
 function update(){
-	canvas.width = video.width;
-	canvas.height = video.height;
-	ctx.clearRect( 0, 0, ctx.canvas.width, ctx.canvas.height);
-	if(video.currentTime < time_start)
+	canvas.width = $(video).width();
+	canvas.height = $(video).height();
+	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+	if (video.currentTime < time_start)
 		video.currentTime = time_start;
-	if(video.currentTime > time_end)
+	if (video.currentTime > time_end)
 		video.currentTime = time_start;
-	let complete_percent = 100*(video.currentTime/video.duration);
-	$(".slider_time_pos").css("left", complete_percent+"%");
+	let complete_percent = 100 * (video.currentTime / video.duration);
+	$(".slider_time_pos").css("left", complete_percent + "%");
 	$(".current_time").text(video.currentTime.toFixed(2));
 	// noinspection JSCheckFunctionSignatures
-	ctx.drawImage(video, 0,0, canvas.width, canvas.height); //TODO: Subimage using crop.
+	ctx.drawImage(video, 0, 0, canvas.width, canvas.height); //TODO: Subimage using crop.
+
+
+	if(crop[0] && crop[1]){
+		let rect = canvas.getBoundingClientRect();
+		let p1 = unscale(crop[0], rect), p2 = unscale(crop[1],rect);
+		let x = Math.min(p1.x, p2.x);
+		let y = Math.min(p1.y, p2.y);
+		let w = Math.abs(p1.x - p2.x);
+		let h = Math.abs(p1.y - p2.y);
+		ctx.strokeStyle="#FF0000";
+		ctx.strokeRect(x, y, w, h);
+	}
+
 	requestAnimationFrame(update.bind(this)); // Tell browser to trigger this method again, next animation frame.
 }
+
+update(); //Start rendering
